6f59e263f766b0e6743a96e7a70fc3ca02aea7a3
aswins2108@gmail.com <aswins2108@gmail.com>
2021-12-29 17:14:18 +0530

    sign in part with the custom-button completed.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUP from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 
 
@@ -16,6 +17,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUP} />
       </Switch>
     </div>
   );
