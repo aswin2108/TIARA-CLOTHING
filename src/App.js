fa6904f6030cff492efafff2f779e4aae4fe2d55
aswins2108@gmail.com <aswins2108@gmail.com>
2022-01-09 20:00:48 +0530

    the data abt the user is being stored on the firestore and on the app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUP from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 
 class App extends React.Component {
@@ -17,16 +17,28 @@ constructor(){
 
   this.state={
      currentUser: null
-  }
+  };
 }
 
 unsubscribeFromAuth=null
 
 componentDidMount(){
-  this.unsubscribeFromAuth=auth.onAuthStateChanged(user=>{
-    this.setState({currentUser: user});
+  this.unsubscribeFromAuth=auth.onAuthStateChanged(async userAuth=>{
+    if(userAuth){
+      const userRef= await createUserProfileDocument(userAuth);
 
-    console.log(user);
+      userRef.onSnapshot(snapShot=>{
+       this.setState({
+         currentUser:{
+           id: snapShot.id,
+           ...snapShot.data()
+         }
+       });
+      });
+    }
+    else{
+      this.setState({currentUser: userAuth});
+    }
   });
 }
 
