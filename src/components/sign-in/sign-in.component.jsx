5c81fb36edb941ea6abe9515aa59fca74fa06230
aswins2108@gmail.com <aswins2108@gmail.com>
2022-01-11 16:35:23 +0530

    bug fix

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -60,7 +60,7 @@ class SignIn extends React.Component{
               required />
               <div className='buttons'>
              <CustomButton type='submit'>Sign in</CustomButton>
-             <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+             <CustomButton onClick={signInWithGoogle} type="button" isGoogleSignIn>
              Sign in with Google
              </CustomButton>
              </div>
