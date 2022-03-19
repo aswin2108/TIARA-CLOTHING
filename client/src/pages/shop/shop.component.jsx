d9135738bfe1d5c7b76e07432f51adad703785b8
aswins2108@gmail.com <aswins2108@gmail.com>
2022-03-19 14:08:39 +0530

    did code spliting using lazy and suspense

--- client/src/pages/shop/shop.component.jsx
+++ client/src/pages/shop/shop.component.jsx
@@ -1,11 +1,14 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, lazy, Suspense} from 'react';
 import {Route} from 'react-router-dom'
-import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
-import CollectionPageContainer from '../collection/collection.container';
 
 import {connect} from 'react-redux';
 
+import Spinner from '../../components/spinner/spinner.component';
+
 import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
+
+const CollectionsOverviewContainer=lazy(()=>import('../../components/collections-overview/collections-overview.container'));
+const CollectionPageContainer=lazy(()=>import('../collection/collection.container'));
 
 
 const ShopPage =({match,fetchCollectionsStart})=>{
@@ -14,9 +17,11 @@ const ShopPage =({match,fetchCollectionsStart})=>{
       },[fetchCollectionsStart]);
        return (
         <div className='shop-page'>
+        <Suspense fallback={<Spinner />}>
           <Route exact path={`${match.path}`} component={CollectionsOverviewContainer} />
           <Route path={`${match.path}/:collectionId`} component={CollectionPageContainer}/>
-        </div>
+        </Suspense>
+          </div>
      );
     
      } 
