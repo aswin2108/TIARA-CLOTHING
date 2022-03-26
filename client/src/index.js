a51b3b631f11726105b0bfdf8bc3abb20ed672f1
aswins2108@gmail.com <aswins2108@gmail.com>
2022-03-26 12:43:10 +0530

    fixing

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
-import * as serviceWorker from './service-worker';
+import * as serviceWorker from './serviceWorker';
 
 import './index.css';
 import App from './App';
