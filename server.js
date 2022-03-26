e20dea6cdb345baeb145464670ef1d8fb9a12aec
aswins2108@gmail.com <aswins2108@gmail.com>
2022-03-26 11:07:48 +0530

    compression has been excluded to check the status of deployment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const compression = require('compression');
+//const compression = require('compression');
 const enforce= require('express-sslify');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(compression());
+  //app.use(compression());
   app.use(enforce.HTTPS({trustProtoHeader: true}));
 
   app.use(express.static(path.join(__dirname, 'client/build')));
